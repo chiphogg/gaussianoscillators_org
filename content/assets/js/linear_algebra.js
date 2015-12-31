61db5164d5c60a7236de7c0747660e76e6e11fae
Chip Hogg <charles.r.hogg@gmail.com>
2015-12-30 21:09:26 -0500

    First working eigendecomposition (vectors only)

--- content/assets/js/linear_algebra.js
+++ content/assets/js/linear_algebra.js
@@ -80,14 +80,49 @@ function LoopingCholesky(M) {
 //
 // Returns:
 //    An array-of-arrays equivalent to the given matrix.
-function DeepCopyMatrixOnly(M) {
-  var matrix = jStat.zeros(M[0].length, M[0][0].length);
-  for (var i = 0; i < matrix.length; i++) {
-    for (var j = 0; j < matrix[i].length; j++) {
-      matrix[i][j] = M[0][i][j];
+function DeepCopy(M) {
+  return jStat.create(M.length, M[0].length, function(i, j) {
+    return M[i][j];
+  });
+}
+
+// Matrix for an n-D Givens rotation about columns i and j by angle theta.
+function GivensRotationMatrix(n, i, j, theta) {
+  var c = Math.cos(theta);
+  var s = Math.sin(theta);
+  var g = jStat.identity(n);
+  g[i][i] = g[j][j] = c;
+  g[i][j] = s;
+  g[j][i] = -s;
+  return g;
+}
+
+// The index of the off-diagonal element with maximal magnitude.
+function IndexOfMaxOffDiagonalInRow(mat, row) {
+  var max_value = undefined;
+  var i_best = -1;
+  for (var i = 0; i < mat[row].length; i++) {
+    if (i == row) { continue; }
+    var value = Math.abs(mat[row][i]);
+    if (typeof max_value === "undefined" || value > max_value) {
+      max_value = value;
+      i_best = i;
     }
   }
-  return matrix;
+  return i_best;
+}
+
+// The "pivot row" is a row which contains a highest off-diagonal element.
+//
+// We assume that the i_columns array gives the index of the highest
+// off-diagonal element for that row.
+function PivotRow(mat, i_columns) {
+  var values = i_columns.map(function(v, i) {
+    return Math.abs(mat[i][v]);
+  });
+  return values.reduce(function(i_max, v, i, a) {
+    return v > a[i_max] ? i : i_max;
+  }, 0);
 }
 
 // Return the eigenvalues and eigenvectors of the matrix.
@@ -115,61 +150,8 @@ function Eigen(M) {
   //     Veselić, SIAM Journal on Matrix Analysis and Applications 1992 13:4,
   //     1204-1245 http://dx.doi.org/10.1137/0613074
 
-  //////////////////////////////////////////////////////////////////////////////
-  // Helper functions.
-
-  // The index of the off-diagonal element with maximal magnitude.
-  function IndexOfMaxOffDiagonalInRow(mat, row) {
-    var max_value = undefined;
-    var i_best = -1;
-    for (var i = 0; i < mat[row].length; i++) {
-      if (i == row) { continue; }
-      var value = Math.abs(mat[row][i]);
-      if (typeof max_value === "undefined" || value > max_value) {
-        max_value = value;
-        i_best = i;
-      }
-    }
-    return i_best;
-  }
-
-  // The "pivot row" is a row which contains a highest off-diagonal element.
-  //
-  // We assume that the i_columns array gives the index of the highest
-  // off-diagonal element for that row.
-  function PivotRow(mat, i_columns) {
-    var values = i_columns.map(function(v, i) {
-      return Math.abs(mat[i][v]);
-    });
-    return values.reduce(function(i_max, v, i, a) {
-      return v > a[i_max] ? i : i_max;
-    }, 0);
-  }
-
-  // Matrix for an n-D Givens rotation about columns i and j by angle theta.
-  function GivensRotationMatrix(n, i, j, theta) {
-    var c = Math.cos(theta);
-    var s = Math.sin(theta);
-
-    var g = jStat.identity(n);
-    g[i][i] = g[j][j] = c;
-    g[i][j] = s;
-    g[j][i] = -s;
-  }
-
-  // Zero out the (i, j) and (j, i) entries, logging them for posterity (so we
-  // can make sure they were already small).
-  function ZeroOutEntries(M, i, j) {
-    console.log('i: ' + i + '; j: ' + j +
-        '; (i, j): ' + M[i][j] + '; (j, i): ' + M[j][i]);
-    M[i][j] = M[j][i] = 0;
-  }
-
-  //////////////////////////////////////////////////////////////////////////////
-  // Variable initializations.
-
   // A copy of the matrix which we can modify without clobbering M.
-  var matrix = DeepCopyMatrixOnly(M);
+  var matrix = DeepCopy(M);
   // The dimension of the (assumed-to-be-square) matrix.
   var n = matrix.length;
   // The matrix which will eventually converge to the eigenvectors of M.
@@ -184,17 +166,37 @@ function Eigen(M) {
   var max_index = matrix.map(function(v, i, a) {
     return IndexOfMaxOffDiagonalInRow(a, i);
   });
+  // The threshold for the highest nonzero element to be considered "close
+  // enough" to zero.
+  var pivot_threshold = 1e-6;
+  // The number of iterations we have taken.
+  var iter = 0;
 
-  //////////////////////////////////////////////////////////////////////////////
-  // Logic.
-
-  while (num_rows_changed > 0) {
+  // Iterate until our pivot is under the threshold n times consecutively.
+  var countdown = n;
+  while (countdown > 0) {
+    iter++;
     // Find the location of the pivot.
     var pivot_row = PivotRow(matrix, max_index);
     var pivot_column = max_index[pivot_row];
-    // Compute the Givens rotation angle.
-    var theta = 0.5 * Math.atan2(2 * matrix[i][j], matrix[j][j] - matrix[i][i]);
+    // Check the end condition.
+    countdown = (Math.abs(matrix[pivot_row][pivot_column]) < pivot_threshold) ?
+      (countdown - 1) : n;
+    // Compute the Givens rotation angle and matrix.
+    var theta = -0.5 * Math.atan2(
+        2 * matrix[pivot_row][pivot_column],
+        matrix[pivot_column][pivot_column] - matrix[pivot_row][pivot_row]);
+    var g_mat = GivensRotationMatrix(n, pivot_row, pivot_column, theta);
+    var g_mat_t = jStat.transpose(g_mat);
+    // Apply the Givens matrix to the input matrix and the eigenvector matrix.
+    matrix = jStat.multiply(g_mat, jStat.multiply(matrix, g_mat_t));
+    matrix[pivot_row][pivot_column] = matrix[pivot_column][pivot_row] = 0;
+    eigenvectors = jStat.multiply(g_mat, eigenvectors);
+    // Update the max_index array.
+    max_index[pivot_row] = IndexOfMaxOffDiagonalInRow(matrix, pivot_row);
+    max_index[pivot_column] = IndexOfMaxOffDiagonalInRow(matrix, pivot_column);
   }
+  console.log("Found eigendecomposition in " + iter + " iterations.");
 
   return {
     vectors: eigenvectors,
